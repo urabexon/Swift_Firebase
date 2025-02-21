a33bc77d031a3333d03d526f1a7633f62374c5d6
urabexon <hiroki708.a48uk@gmail.com>
2025-02-22 08:38:33 +0900

    Initial Commit

---
+++ Photo/PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoApp.swift
+//  Photo
+//
+//  Created by 卜部大輝 on 2025/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
