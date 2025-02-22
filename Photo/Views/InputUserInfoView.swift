fbb6a4fa6370b34490fc08ab9a03a2156f4bd8b7
urabexon <hiroki708.a48uk@gmail.com>
2025-02-22 11:35:53 +0900

    InputUserInfo fix Dir fix

---
+++ Photo/Views/InputUserInfoView.swift
@@ -0,0 +1,54 @@
+//
+//  InputUserInfoView.swift
+//  Photo
+//
+//  Created by Hiroki Urabe on 2025/02/22.
+//
+
+import SwiftUI
+
+struct InputUserInfoView: View {
+    @State private var name = "" // テキストフィールドに入力された名前を受け取る
+    var body: some View {
+        NavigationStack {
+            // この画面の説明文を表示
+            Text("ユーザー画像の設定と名前の入力をしてください")
+                .font(.subheadline)
+                .padding(.horizontal, 30)
+            
+            // ユーザーのアイコン画像になるもの
+            Image("noimage")
+                .resizable()
+                .scaledToFit()
+                .clipShape(.rect(cornerRadius: 10))
+                .padding(30)
+            
+            // 名前入力欄と登録ボタンを横並びにするためにでHStackを使用
+            HStack {
+                // ユーザー名を入力するテキストフィールド
+                TextField("名前を入力してください", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                // ユーザー名とユーザーのアイコン画像を登録するボタン
+                Button {
+                    // ボタンが押されたら実行: 登録の処理
+                    submitUserInfo()
+                } label: {
+                    Text("登録")
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding(.horizontal, 30)
+        }
+    }
+    // ユーザー情報を登録する処理
+    func submitUserInfo() {
+        if name.isEmpty {
+            print("名前が入力されてないよ")
+            return
+        }
+    }
+}
+
+#Preview {
+    InputUserInfoView()
+}
