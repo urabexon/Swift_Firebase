ad6de25c2a991999c1aef385c24581bc811bd229
urabexon <hiroki708.a48uk@gmail.com>
2025-02-22 16:34:27 +0900

    CreatePost fix

--- Photo/Views/CreatePostView.swift
+++ Photo/Views/CreatePostView.swift
@@ -8,41 +8,47 @@
 import SwiftUI
 
 struct CreatePostView: View {
+    @Environment(\.dismiss) private var dismiss // 投稿したら前の画面に戻る用
+    @ObservedObject var viewModel: TimelineViewModel
     let userInfoHeight = ScreenInfo.width * 0.2
     let postWidth = ScreenInfo.width * 0.9
-    
-    // テスト表示
-    let userName = "User Name"
-    let userIconName = "noimage"
-    
     @State var postImage: UIImage?
     @State var postText = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         // オレンジのVStack
         VStack {
             // 青のHStack
             HStack {
-                Image(userIconName)
+                Image(uiImage: viewModel.user.iconImage)
                     .resizable()
                     .scaledToFit()
                     .clipShape(.rect(cornerRadius: 5))
                     .frame(width: userInfoHeight, height: userInfoHeight)
                 
-                Text(userName)
+                Text(viewModel.user.name)
                     .font(.headline)
                 Spacer()
             }
             .frame(maxWidth: postWidth ,maxHeight: userInfoHeight)
-            .background(.blue)
-            
             ImageCaptureView(capturedImage: $postImage)
                 .padding(.vertical ,30)
             TextField("投稿する内容を入力してください", text: $postText)
                 .textFieldStyle(.roundedBorder)
             // 投稿ボタン
             Button {
-                // 投稿処理
+                if postText.isEmpty {
+                    showAlert = true
+                } else {
+                    DispatchQueue.main.async {
+                        // postでFirebaseRealtimeDBに投稿する処理
+                        viewModel.sendPost(
+                            postImage: postImage ?? UIImage(named: "noimage")!,
+                            postText: postText)
+                        dismiss()
+                    }
+                }
             } label: {
                 Text("投稿")
             }
@@ -50,10 +56,13 @@ struct CreatePostView: View {
             .padding()
         }
         .frame(maxWidth: postWidth)
-        .background(.orange)
+        .alert("投稿の本文を入力してください", isPresented: $showAlert) {
+            Text("OK")
+        }
     }
 }
 
 #Preview {
-    CreatePostView()
+    @Previewable @StateObject var viewModel = TimelineViewModel()
+    CreatePostView(viewModel: viewModel)
 }
